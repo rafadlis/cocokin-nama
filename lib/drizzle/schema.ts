d8e8388c8b129b5e466c1ed1dca51c81eb048b13
rafadlis <rahmatfadlis@gmail.com>
2024-12-08 17:38:53 +0700

    feat: change to proper schema

--- lib/drizzle/schema.ts
+++ lib/drizzle/schema.ts
@@ -1,12 +1,4 @@
-import { pgTable, varchar, serial } from "drizzle-orm/pg-core";
-
-export const DaftarNamaMuridTable = pgTable("daftar_nama_murid", {
-  id: serial("id").primaryKey(),
-  name: varchar("name", { length: 255 }).notNull(),
-  kelas: varchar("kelas", { length: 255 }).notNull(),
-  nomor_absen: varchar("nomor_absen", { length: 255 }).notNull(),
-  nama_lain_id: serial("nama_lain_id").references(() => NamaLainTable.id),
-});
+import { pgTable, varchar, serial, integer } from "drizzle-orm/pg-core";
 
 export const NamaLainTable = pgTable("nama_lain", {
   id: serial("id").primaryKey(),
@@ -14,8 +6,16 @@ export const NamaLainTable = pgTable("nama_lain", {
   kelas: varchar("kelas", { length: 255 }).notNull(),
 });
 
+export const DaftarNamaMuridTable = pgTable("daftar_nama_murid", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  kelas: varchar("kelas", { length: 255 }).notNull(),
+  nomor_absen: varchar("nomor_absen", { length: 255 }).notNull(),
+  nama_lain_id: integer("nama_lain_id").references(() => NamaLainTable.id),
+});
+
 export const NilaiTable = pgTable("nilai", {
   id: serial("id").primaryKey(),
-  nilai: varchar("nilai", { length: 255 }).notNull(),
-  murid_id: serial("murid_id").references(() => DaftarNamaMuridTable.id),
+  benar: integer("benar").notNull(),
+  nama_lain_id: integer("nama_lain_id").references(() => NamaLainTable.id),
 });
