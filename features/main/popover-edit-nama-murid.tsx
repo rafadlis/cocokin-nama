d30467349c95f0f36ed01bacec54c2dbfd045f63
rafadlis <rahmatfadlis@gmail.com>
2024-12-09 10:53:49 +0700

    feat: use toast sonner promise for updating name

--- features/main/popover-edit-nama-murid.tsx
+++ features/main/popover-edit-nama-murid.tsx
@@ -32,22 +32,32 @@ export function PopoverEditNamaMurid({
   const debouncedSearch = useDebouncedCallback((value: string) => {
     router.push(`?${q ? `q=${q}&` : ""}search=${value}`, { scroll: false });
   }, 300);
+
   async function updateNamaLainMuridAction(
     muridId: number,
     namaLainId: number
   ) {
-    await updateNamaLainMurid(muridId, namaLainId).then((result) => {
-      if (result) {
-        toast.success("Nama murid berhasil diubah");
-      } else {
-        toast.error("Nama murid gagal diubah");
-      }
-      router.push(`?${q ? `q=${q}&` : ""}search=`, { scroll: false });
-      router.refresh();
-
-      setOpen(false);
+    toast.promise(updateNamaLainMurid(muridId, namaLainId), {
+      loading: "Mengubah nama murid...",
+      success: () => {
+        router.push(`?${q ? `q=${q}&` : ""}search=`, { scroll: false });
+        router.refresh();
+        setOpen(false);
+        return "Nama murid berhasil diubah";
+      },
+      error: "Nama murid gagal diubah",
     });
   }
+
+  //   const promise = () => new Promise((resolve) => setTimeout(() => resolve({ name: 'Sonner' }), 2000));
+
+  // toast.promise(promise, {
+  //   loading: 'Loading...',
+  //   success: (data) => {
+  //     return `${data.name} toast has been added`;
+  //   },
+  //   error: 'Error',
+  // });
 
   const [open, setOpen] = useState(false);
   return (
