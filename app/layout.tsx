3fd4b00c94575ff4af578106cd93b0502ae0c42a
rafadlis <rahmatfadlis@gmail.com>
2024-12-09 10:29:34 +0700

    feat: add Toaster component for notifications in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Cocokin Nama",
@@ -13,7 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <Toaster richColors position="top-center" />
+      </body>
     </html>
   );
 }
